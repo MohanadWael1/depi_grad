0b62d2cce9cb8ccd36a1be2c1abc9dd5a09b4d20
Mahmoud Maher <mahmoudmaher2033@gmail.com>
2024-10-09 23:42:45 +0300

    _

--- src/main/java/pages.java
+++ src/main/java/pages.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class pages {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver() ; 
+        WebDriver driver = new ChromeDriver() ;
+        //Maher
     }
 }
