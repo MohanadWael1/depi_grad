5f08e934ab6f3c3af23e4c6a07bab0eee806e155
Mahmoud Maher <mahmoudmaher2033@gmail.com>
2024-10-09 23:44:36 +0300

    _

--- src/main/java/pages.java
+++ src/main/java/pages.java
@@ -4,6 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class pages {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver() ;
-        //Maher
+        driver.manage().window().maximize();
     }
 }
