30e964320431549af0f53a3bbc4214e8f2dd9734
Mahmoud Maher <mahmoudmaher2033@gmail.com>
2024-10-17 20:59:55 +0300

    Screanshots

--- src/test/java/StepDefs/Hooks.java
+++ src/test/java/StepDefs/Hooks.java
@@ -1,7 +1,11 @@
 package StepDefs;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,5 +24,11 @@ public class Hooks {
     public void CloseBrowser()
     {
         driver.quit();
+    }
+
+    @AfterStep
+    public static void TakeScreenshot (Scenario scenario){
+        final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", "image");
     }
 }
